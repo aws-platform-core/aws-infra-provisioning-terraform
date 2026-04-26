152cec4f14ac380464ad1872d80200292d828cbb
Kiran Kumar V <rockiran.2525@gmail.com>
2026-04-26 15:50:21 +0200

    Add Terraform stack for req-1777211418698

---
+++ stacks/req-1777211418698/variables.tf
@@ -0,0 +1,20 @@
+variable "function_name" {
+    type = any
+  }
+  
+variable "environment" {
+    type = any
+  }
+  
+variable "versioning" {
+    type = any
+  }
+  
+variable "cors" {
+    type = any
+  }
+  
+variable "bucket_name" {
+    type = any
+  }
+  

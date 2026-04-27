04f85c0d136a0f89ab751a93d85b9f0857326483
Kiran Kumar V <rockiran.2525@gmail.com>
2026-04-27 11:03:44 +0200

    Add Terraform stack for req-1777280622566

---
+++ stacks/req-1777280622566/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "versioning" {
+  type = bool
+}
